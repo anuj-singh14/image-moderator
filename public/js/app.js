b7d0bf924febcc00b03b73cf8ed6e2a99bc02818
Gaharana <mr.priyanshgaharana@gmail.com>
2020-05-10 13:09:05 +0530

    Revert "show output with html tags"
    This reverts commit 7fe88f54608522531a557c6c4da5b4569fd723fb.

--- public/js/app.js
+++ public/js/app.js
@@ -2,34 +2,22 @@ const imageForm = document.querySelector('form')
 const imageurl = document.querySelector('input')
 const pre = document.querySelector('#json')
 // console.log('Hello')
-const output = document.querySelector('#output');
 
 imageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const url = imageurl.value
-    output.innerHTML='Loading...'
-    fetch('/moderator?imageurl=' +url)
-    .then(response=>response.json())
-    .then(data=>{
-        output.innerHTML='';
-        if(data.error) output.innerHTML=data.error;
-        for(let i=0;i<data.length;i++){
-            let newp=document.createElement('p');
-            newp.innerHTML=data[i];
-            output.appendChild(newp);
-        }        
+
+    pre.textContent = 'Loading...'
+    
+    fetch('/moderator?imageurl=' + url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return pre.textContent = data.error
+            }
+
+            pre.textContent = JSON.stringify(data, undefined, 2)
+        })
     })
-    // fetch('/moderator?imageurl=' + url).then((response) => {
-    //     response.json().then((data) => {
-    //         if (data.error) {
-    //             return pre.textContent = data.error
-    //         }
-            
-
-    //         pre.textContent = JSON.stringify(data, undefined, 2)
-    //     })
-    // })
-
 })
 
 // weatherForm.addEventListener('submit', (e) => {
