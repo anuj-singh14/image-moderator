7fe88f54608522531a557c6c4da5b4569fd723fb
Gaharana <mr.priyanshgaharana@gmail.com>
2020-05-10 12:54:01 +0530

    show output with html tags

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,34 @@ const imageForm = document.querySelector('form')
 const imageurl = document.querySelector('input')
 const pre = document.querySelector('#json')
 // console.log('Hello')
+const output = document.querySelector('#output');
 
 imageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const url = imageurl.value
-
-    pre.textContent = 'Loading...'
-    
-    fetch('/moderator?imageurl=' + url).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                return pre.textContent = data.error
-            }
-
-            pre.textContent = JSON.stringify(data, undefined, 2)
-        })
+    output.innerHTML='Loading...'
+    fetch('/moderator?imageurl=' +url)
+    .then(response=>response.json())
+    .then(data=>{
+        output.innerHTML='';
+        if(data.error) output.innerHTML=data.error;
+        for(let i=0;i<data.length;i++){
+            let newp=document.createElement('p');
+            newp.innerHTML=data[i];
+            output.appendChild(newp);
+        }        
     })
+    // fetch('/moderator?imageurl=' + url).then((response) => {
+    //     response.json().then((data) => {
+    //         if (data.error) {
+    //             return pre.textContent = data.error
+    //         }
+            
+
+    //         pre.textContent = JSON.stringify(data, undefined, 2)
+    //     })
+    // })
+
 })
 
 // weatherForm.addEventListener('submit', (e) => {
