29a47b1df04391627f8baab6155dc439a0b03ed4
Anuj Singh <anujs787@gmail.com>
2020-05-14 11:44:35 +0530

    Removed commented code

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,11 @@
 const imageForm = document.querySelector('form')
 const imageurl = document.querySelector('input')
-const pre = document.querySelector('#json')
 const output = document.querySelector('#output');
 const img = document.querySelector('.portrait')
 
 imageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const url = imageurl.value
-    // img.src = "data:,"
 
     output.innerHTML='Loading...';
     fetch('/moderator?imageurl=' + url).then((response) => {
@@ -24,8 +22,6 @@ imageForm.addEventListener('submit', (e) => {
                 newptag.innerHTML=result;
                 output.appendChild(newptag);
             })
-
-            // pre.textContent = JSON.stringify(data, undefined, 2)
         })
     })
 })
