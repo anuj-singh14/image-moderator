1492cc7efb1c20eb024b8e566cdccc09405f1506
Anuj Singh <anujs787@gmail.com>
2020-07-13 13:12:56 +0530

    Fixed indentation and made API key private

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,28 @@
-const imageForm = document.querySelector('form')
-const imageurl = document.querySelector('input')
+const imageForm = document.querySelector('form');
+const imageurl = document.querySelector('input');
 const output = document.querySelector('#output');
-const img = document.querySelector('.portrait')
+const img = document.querySelector('.portrait');
 
 imageForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const url = imageurl.value;
-    img.style.display = 'none';
-    output.innerHTML = 'Loading...';
-    fetch('/moderator?imageurl=' + url).then((response) => {
-        response.json().then((data) => {
-            output.innerHTML = '';
-            if (data.error) {
-                output.innerHTML = data.error;
-                img.src = null;
-                return
-            }
-            img.src = url;
-            img.style.display = '';
-            data.map(result => {
-                const newptag = document.createElement('p');
-                newptag.innerHTML = result;
-                output.appendChild(newptag);
-            })
-        })
-    })
-})
+  e.preventDefault();
+  const url = imageurl.value;
+  img.style.display = 'none';
+  output.innerHTML = 'Loading...';
+  fetch('/moderator?imageurl=' + url).then((response) => {
+    response.json().then((data) => {
+      output.innerHTML = '';
+      if (data.error) {
+        output.innerHTML = data.error;
+        img.src = null;
+        return;
+      }
+      img.src = url;
+      img.style.display = '';
+      data.map((result) => {
+        const newptag = document.createElement('p');
+        newptag.innerHTML = result;
+        output.appendChild(newptag);
+      });
+    });
+  });
+});
